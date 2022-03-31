ffefb3fa58c23e1ea95b9eed8f53f7f39ec4055f
Erdal Nayir <erdal.nayir2001@gmail.com>
2022-03-31 13:07:46 +0300

    Add files via upload

---
+++ CarProject/CarProject/CarOperations/GetCars/GetByIdQuery.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using CarProject.DbOperations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarProject.CarOperations.GetCars
+{
+    public class GetByIdQuery
+    {
+        private readonly CarStoreDbContext _context;
+        private readonly IMapper _mapper;
+
+        public int CarId { get; set; }
+        public GetByIdQuery(CarStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public CarViewModel Handle()
+        {
+            var car= _context.Cars.SingleOrDefault(c => c.Id == CarId);
+
+            var result = _mapper.Map<CarViewModel>(car);
+        
+            return result;
+            
+        }
+
+        public class CarViewModel
+        {
+            public string? BrandName { get; set; }
+            public string? ColorName { get; set; }
+            public int ModelYear { get; set; }
+            public int Price { get; set; }
+            public string? MotorType { get; set; }
+        }
+    }
+}
---
+++ CarProject/CarProject/Common/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CarProject.CarOperations;
+using CarProject.CarOperations.GetCars;
+using CarProject.Entity.Concrete;
+using static CarProject.CarOperations.GetCars.GetByIdQuery;
+
+namespace CarProject.Common
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CreateCarModel, Car>();
+            CreateMap<Car, CarViewModel>();
+            CreateMap<Car, CarsViewModel>();
+        }
+
+        
+    }
+}
